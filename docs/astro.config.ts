291d81e2a4216c93c9b335e44d66ce73240c6267
Adrian Webb <adrian@webb.sh>
2026-09-15 14:36:55 -0400

    Remove duplicate repository content authority (#60)

--- docs/astro.config.ts
+++
@@ -1,8 +0,0 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-process.env.TREESEED_TENANT_ROOT ??= dirname(fileURLToPath(import.meta.url));
-
-const { createTenantSite } = await import('@treeseed/core/config');
-
-export default createTenantSite();
